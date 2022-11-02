1a70b661875aa22f2dfe8a9bb795b17db9a9a1e7
glf <myq0glf@163.com>
2022-11-02 12:46:16 +0800

    add lateral control

--- src/Sample/reference_line.h
+++ src/Sample/reference_line.h
@@ -20,15 +20,18 @@ public:
 	void findIndex(PanoSimSensorBus::Lidar_ObjList_G* pLidar);
 	// 存储中心点的索引
 	void calcCenterPoint() {
-		//std::cout << "inner num: " << this->inner << std::endl;
-		for (int i = 0; i < this->inner; ++i) {
+		for (int i = 0; i < in_xy.size(); ++i) {
 			center_point_xy.emplace_back((out_xy[i].first + in_xy[match_point_index_set[i]].first) / 2, 
 				(out_xy[i].second + in_xy[match_point_index_set[i]].second) / 2);
 		}
-		//std::cout << center_point_xy[127].first << std::endl;
 	}
 	void average_interpolation(Eigen::MatrixXd &input, std::vector<std::pair<double, double>> &output, double interval_dis,
 		double distance);
+
+	std::vector<std::pair<double, double>> get_in_xy() {
+		return in_xy;
+	}
+
 	std::vector<std::pair<double, double>> get_center_point_xy() {
 		return center_point_xy;
 	}
@@ -36,12 +39,16 @@ public:
 	std::vector<std::pair<double, double>> get_center_point_xy_final() {
 		return center_point_xy_final;
 	}
+
+	void set_center_point_xy_final(std::vector<std::pair<double, double>> center) {
+		center_point_xy_final = center;
+	}
 private:
 	std::vector<std::pair<double, double>> center_point_xy; // 存储参考线的x y
 	std::vector<std::pair<double, double>> center_point_xy_final; // 存储经过插值参考线的x y
 	std::vector<std::pair<double, double>> in_xy;// 将内圈坐标存储下来
 	std::vector<std::pair<double, double>> out_xy;// 将外圈坐标存储下来
-	std::vector<int> match_point_index_set;// 外圈对应距离最近的内圈index
+	int match_point_index_set[200];// 外圈对应距离最近的内圈index
 	int inner = 0;// 内圈锥桶数
 	int outter = 0;// 外圈锥桶数
 
